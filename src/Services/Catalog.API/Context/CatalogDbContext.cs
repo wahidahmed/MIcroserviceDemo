c4c6e39e0c516d2b68930f7a64e357182df47775
wahidahmed <wahidahmed1992@gmail.com>
2023-12-27 09:45:55 +0600

    catalog api context

---
+++ src/Services/Catalog.API/Context/CatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoRepo.Context;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static IConfiguration configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", true, true).Build();
+        static string connectionString = configuration.GetConnectionString("Catalog.API");
+        static string databaseName = configuration.GetValue<string>("DatabaseName");
+        public CatalogDbContext() : base(connectionString, databaseName)
+        {
+        }
+    }
+}
---
+++ src/Services/Catalog.API/Context/CatalogDbContextSeed.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Manager;
+
+namespace Catalog.API.Context
+{
+    public class CatalogDbContextSeed
+    {
+        static ProductManager _productManager = new ProductManager();
+
+        public static void Seed()
+        {
+            var product = _productManager.GetFirstOrDefault(c => true);
+            if (product == null)
+            {
+
+            }
+        }
+    }
+}
